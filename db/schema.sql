236dde716b3653df41acb1cee4f7f75056199ce7
Houston Crosby <houstoncrosby@yahoo.com>
2017-08-14 19:13:43 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (100),
+    devoured BOOLEAN DEFAULT false,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+)
+    
+SELECT * FROM burgers
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Texas BBQ Burger");
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger");
